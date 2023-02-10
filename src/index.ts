db68becd63c665e4c0a91267f1b9105bf0ff56e8
Jakub Piotr Siemaszko <jacob@X-Breeze.com>
2023-02-10 12:47:34 +0100

    Adjusted tsconfig to include DOM module which contains fetch function. Added example of a rendering mustache template (without external files)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Runnning app...');
+const Mustache = require('mustache');
+
+//Simple example model - without using JSON file
+const model = {
+    name: "ExampleSource"
+}
+
+//Simple template - without external template file
+const template = `
+    --Create Database 
+    CREATE DATABASE {{name}};
+    GO`;
+
+function loadTemplate(){
+      var toRender = Mustache.render(template, model);
+      console.log(toRender);
+}
+
+loadTemplate();
