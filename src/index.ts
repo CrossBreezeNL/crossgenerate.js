18e8864e2a89b2428bdc4babad629997e0bb5bbb
Jakub Piotr Siemaszko <jacob@X-Breeze.com>
2023-02-17 15:48:45 +0100

    Small adjustments and comments.

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,23 @@ const templateFilePath = './templates/CreateDatabase.mst';
 const modelFilePath = './model/example-source.json';
 const outFilePath = './out/result.txt';
 
-async function loadTemplate(filePath: string): Promise<string> {
+//Loads file from a path.
+//Used for loading the template and model files.
+async function loadFile(filePath: string): Promise<string> {
   return await fs.promises.readFile(filePath, 'utf8');
 };
 
-async function loadModel(filePath: string): Promise<string> {
-  return await fs.promises.readFile(filePath, 'utf8');
-};
-
+//Given a model and a template we parse a mustache template
+//We are using a wrapper to decouple template engine from generation later.
 function parseTemplate(template : string, model: Object): string {
-    return Mustache.render(template, model);    
+    return Mustache.render(template, model);
 };
 
 async function getResult() {
-  let template = await loadTemplate(templateFilePath);
-  let model = JSON.parse(await loadModel(modelFilePath));
+  let template = await loadFile(templateFilePath);
+  let model = JSON.parse(await loadFile(modelFilePath));
   
   saveToFile(outFilePath, parseTemplate(template, model));
-  // console.log();
 };
 
 getResult();
